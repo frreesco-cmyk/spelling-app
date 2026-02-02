c50e14b417df94c06ccb97f6abc603c6dd2b9815
frreesco-cmyk <frreesco@gmail.com>
2026-02-02 12:17:18 +0300

    app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from datetime import datetime
 import time
 
 # --- ИНИЦИАЛИЗАЦИЯ ---
-st.set_page_config(page_title="SPELLING TOTAL CONTROL v7", layout="wide")
+st.set_page_config(page_title="SPELLING CONTROL v8", layout="wide")
 
 def get_db():
-    conn = sqlite3.connect('team_v7_final.db', check_same_thread=False)
+    conn = sqlite3.connect('team_v8_final.db', check_same_thread=False)
     return conn
 
 conn = get_db()
@@ -23,8 +23,7 @@ conn.commit()
 st.markdown("""
 <style>
     .stApp { background-color: #050505; color: #00ffcc; }
-    .stButton>button { border-radius: 5px; font-weight: bold; width: 100%; border: none; }
-    .status-btn { height: 40px; margin-bottom: 10px; }
+    .stButton>button { border-radius: 5px; font-weight: bold; width: 100%; border: none; height: 45px; }
     .online-tag { background: #00ff00; color: black; padding: 3px 10px; border-radius: 10px; font-weight: bold; }
     .afk-tag { background: #ffff00; color: black; padding: 3px 10px; border-radius: 10px; font-weight: bold; }
     .offline-tag { background: #555; color: white; padding: 3px 10px; border-radius: 10px; font-weight: bold; }
@@ -36,7 +35,7 @@ if 'user' not in st.session_state: st.session_state.user = None
 
 # --- ВХОД / РЕГИСТРАЦИЯ ---
 if st.session_state.user is None:
-    st.title("⚡ SPELLING SYSTEM v7")
+    st.title("⚡ SPELLING SYSTEM v8")
     t1, t2 = st.tabs(["ВХОД", "РЕГИСТРАЦИЯ"])
     with t1:
         u = st.text_input("Логин")
@@ -44,4 +43,5 @@ if st.session_state.user is None:
         if st.button("ВОЙТИ"):
             res = cursor.execute("SELECT role, status FROM users WHERE username=? AND password=?", (u, p)).fetchone()
             if res:
-                if res[1]
+                if res[1] == "banned":
+                    st.error
